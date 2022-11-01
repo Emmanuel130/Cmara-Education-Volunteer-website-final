fb799527879314ebdccf372d759d3974bfe3f8e5
Amanuel Detebo <85768416+Emmanuel130@users.noreply.github.com>
2022-11-01 09:35:38 +0300

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -86,7 +86,7 @@ $(document).ready(function() {
 
       else if(index!=1){
         $('.header-links a').each(function(){
-          $(this).css('color','black')
+          $(this).css('color','white')
         });
       }
 
